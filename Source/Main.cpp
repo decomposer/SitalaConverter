81321c0587263ad7c4e223b89624af38c71baf1b
Scott Wheeler <scott@directededge.com>
2020-08-26 06:12:29 +0200

    Don't convert files with more than 16 samples
    This also adds some additional output and fixes the main return values.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -8,24 +8,31 @@ int main(int argc, char* argv[])
     if(argc != 3)
     {
         DBG("Usage: SamplesFromMarsConverter [ADG-File] [Sitala-File]");
-        return -1;
+        return 1;
     }
 
     File adgFile(argv[1]);
     if(!adgFile.exists())
     {
         DBG("ADG file not found");
-        return -1;
+        return 2;
     }
 
     ADGReader reader(adgFile);
     const auto files = reader.getContainSamplePaths();
+
+    if(files.size() > 16)
+    {
+        DBG("Too many samples (" << files.size() << "): " << argv[1]);
+        return 3;
+    }
+
     for(auto file : files)
     {
         if(!file.exists())
         {
             DBG("File " << file.getFullPathName() << " doesn't exist");
-            return -1;
+            return 4;
         }
     }
 
@@ -34,8 +41,10 @@ int main(int argc, char* argv[])
     if(!generator.run())
     {
         DBG("Error generating kit");
-        return -1;
+        return 5;
     }
+
+    DBG("Created: " << argv[2]);
 
     return 0;
 }
