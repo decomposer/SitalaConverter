74f6d2b9c6b3c2dae7766b7ea59c87e265599f75
Scott Wheeler <scott@directededge.com>
2020-08-26 06:48:12 +0200

    Add Ruby script to find appropriate kit files

---
+++ convert_all_kits.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+Dir.glob("*/Ableton Live/**/*Kits/*.adg").each do |kit|
+  sitala_kit = kit.sub('/Ableton Live/', '/Sitala/').sub(/.adg$/, '.sitala')
+  sitala_dir = sitala_kit.sub(/\/[^\/]+\.sitala$/, '')
+  FileUtils.mkdir_p(sitala_dir)
+  system('SamplesFromMarsConverter', kit, sitala_kit)
+end
