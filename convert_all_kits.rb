8e64bce314b92c69122ef6cb236ad7c87497bd0d
Scott Wheeler <scott@directededge.com>
2020-08-26 07:16:34 +0200

    This is the dir structure used by Battery kits

--- convert_all_kits.rb
+++ convert_all_kits.rb
@@ -3,7 +3,9 @@
 require 'fileutils'
 
 Dir.glob("*/Ableton Live/**/*Kits/*.adg").each do |kit|
-  sitala_kit = kit.sub('/Ableton Live/', '/Sitala/').sub(/.adg$/, '.sitala')
+  sitala_kit = kit.sub('/Ableton Live/', '/Sitala/')
+                  .sub(/\/Presets\/[\d\. ]*Kits\//, '/')
+                  .sub(/.adg$/, '.sitala')
   sitala_dir = sitala_kit.sub(/\/[^\/]+\.sitala$/, '')
   FileUtils.mkdir_p(sitala_dir)
   system('SamplesFromMarsConverter', kit, sitala_kit)
