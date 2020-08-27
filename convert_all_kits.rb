2f0907a4df8bf5d115206507d9cc48aec473113c
Scott Wheeler <scott@directededge.com>
2020-08-27 04:09:55 +0200

    Better kit filter

--- convert_all_kits.rb
+++ convert_all_kits.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 
-Dir.glob("*/Ableton Live/**/**/*Kits/**/*.adg").each do |kit|
+Dir.glob("*/Ableton Live/**/**/*.adg").select { |k| k.include?('Kit') }.each do |kit|
   kit = File.expand_path(kit)
   sitala_kit = kit.sub('/Ableton Live/', '/Sitala/')
                   .sub(/\/Presets\/[\d\. ]*Kits\//, '/')
