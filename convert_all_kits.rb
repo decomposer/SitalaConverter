8d384512e56ed84305e6e664ae00453d57f6623e
Scott Wheeler <scott@directededge.com>
2020-08-27 18:02:06 +0200

    Use shorter paths for Sitala
    This breaks away from the Ableton convention where there are different
    kinds of files, and follows more closely the way that things are done
    with Battery.

--- convert_all_kits.rb
+++ convert_all_kits.rb
@@ -3,11 +3,12 @@
 require 'fileutils'
 
 Dir.glob("*/Ableton*/**/**/*.adg").select { |k| k.include?('Kit') }.each do |kit|
+  path_components = kit.sub(/\/Ableton( Live){0,1}\//, '/Sitala/')
+                      .sub(/.adg$/, '.sitala')
+                      .split(File::SEPARATOR)
+  FileUtils.mkdir_p(path_components[0,3].join(File::SEPARATOR))
   kit = File.expand_path(kit)
-  sitala_kit = kit.sub(/\/Ableton( Live){0,1}\//, '/Sitala/')
-                  .sub(/\/Presets\/[\d\. ]*Kits\//, '/')
-                  .sub(/.adg$/, '.sitala')
-  sitala_dir = sitala_kit.sub(/\/[^\/]+\.sitala$/, '')
-  FileUtils.mkdir_p(sitala_dir)
+  sitala_kit = File.expand_path(
+    [*path_components[0,3], path_components.last].join(File::SEPARATOR))
   system('SamplesFromMarsConverter', kit, sitala_kit)
 end
