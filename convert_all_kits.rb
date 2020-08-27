a07c720d7c1ac694ba84b07e1955f3744a822262
Scott Wheeler <scott@directededge.com>
2020-08-27 05:25:23 +0200

    Lindrum uses "Ableton" and not "Ableton Live"

--- convert_all_kits.rb
+++ convert_all_kits.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 
-Dir.glob("*/Ableton Live/**/**/*.adg").select { |k| k.include?('Kit') }.each do |kit|
+Dir.glob("*/Ableton*/**/**/*.adg").select { |k| k.include?('Kit') }.each do |kit|
   kit = File.expand_path(kit)
   sitala_kit = kit.sub('/Ableton Live/', '/Sitala/')
                   .sub(/\/Presets\/[\d\. ]*Kits\//, '/')
