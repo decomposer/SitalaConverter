db3aced37340f015b08a35786ffa2e09ea883c03
Scott Wheeler <scott@directededge.com>
2020-08-27 15:40:16 +0200

    Account for it only being Ableton and not Ableton Live here too

--- convert_all_kits.rb
+++ convert_all_kits.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 
 Dir.glob("*/Ableton*/**/**/*.adg").select { |k| k.include?('Kit') }.each do |kit|
   kit = File.expand_path(kit)
-  sitala_kit = kit.sub('/Ableton Live/', '/Sitala/')
+  sitala_kit = kit.sub(/\/Ableton( Live){0,1}\//, '/Sitala/')
                   .sub(/\/Presets\/[\d\. ]*Kits\//, '/')
                   .sub(/.adg$/, '.sitala')
   sitala_dir = sitala_kit.sub(/\/[^\/]+\.sitala$/, '')
