348449eae3118d911fcea417878e8d301347b463
Scott Wheeler <scott@directededge.com>
2022-06-24 13:23:34 +0200

    Indicate that files are droppable

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -12,7 +12,13 @@ public:
     void resized() override;
 
     bool isInterestedInFileDrag(const StringArray &files) override;
+    void fileDragEnter(const StringArray &files, int x, int y) override;
+    void fileDragExit(const StringArray &files) override;
     void filesDropped(const StringArray &files, int x, int y) override;
 private:
+    void setDragging(bool d);
+
+    bool m_dragging = false;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
