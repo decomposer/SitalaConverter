144e1f3bffcfc636f4bcc79a32f9170e2d314582
Scott Wheeler <scott@directededge.com>
2022-08-30 09:52:27 +0200

    Let user select output directory

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,9 +2,17 @@
 #include "AbletonDeviceGroupReader.h"
 #include "SitalaKitGenerator.h"
 
-MainComponent::Label::Label(const String &text) : juce::Label(String(), text)
+MainComponent::Label::Label(const String &text) :
+    juce::Label(String(), text),
+    m_textColour(findColour(Label::textColourId))
 {
     setSizeHint(LayoutManager::Orientation::Vertical, Constraints::fixed(Drawing::ControlHeight));
+
+}
+
+void MainComponent::Label::setEnabled(bool enabled)
+{
+    setColour(Label::textColourId, enabled ? m_textColour : m_textColour.withAlpha(0.3f));
 }
 
 MainComponent::ToggleButton::ToggleButton(const String &text, bool checked) : juce::ToggleButton(text)
@@ -18,13 +26,15 @@ enum
     DirectoryButtonGroup = 1001
 };
 
+static const auto noneSelected = TRANS("None selected");
+
 MainComponent::MainComponent() :
     LayoutManagedComponent(Orientation::Vertical),
     m_dropLabel(TRANS("Drop Ableton Live devices here:")),
     m_embedButton(TRANS("Embed samples in kit"), true),
     m_sameDirectoryButton(TRANS("Put Sitala kits into same folder as the Ableton kit"), true),
     m_specificDirectoryButton(TRANS("Put Sitala kits into a specific folder")),
-    m_directoryLabel(TRANS("None selected"))
+    m_directoryLabel(noneSelected)
 {
     setSize(400, 300);
     setBorderSizes(BorderSize(10));
@@ -40,11 +50,41 @@ MainComponent::MainComponent() :
     addSpacer(Constraints::fixed(Drawing::ControlHeight));
 
     m_sameDirectoryButton.setRadioGroupId(DirectoryButtonGroup);
+    m_sameDirectoryButton.onClick = [this] {
+        m_directoryLabel.setEnabled(false);
+        m_directoryLabel.setText(noneSelected, dontSendNotification);
+    };
+
     appendComponent(&m_sameDirectoryButton);
+
+    m_directoryLabel.setEnabled(false);
+    auto directoryLabelBorderSize = m_directoryLabel.getBorderSize();
+    directoryLabelBorderSize.setLeft(26);
+    m_directoryLabel.setBorderSize(directoryLabelBorderSize);
 
     m_specificDirectoryButton.setRadioGroupId(DirectoryButtonGroup);
     appendComponent(&m_specificDirectoryButton);
     appendComponent(&m_directoryLabel);
+
+    m_specificDirectoryButton.onClick = [this] {
+        if(!m_specificDirectoryButton.getToggleState())
+        {
+            return;
+        }
+
+        FileChooser chooser(TRANS("Select folder to place kits..."));
+        if(chooser.browseForDirectory())
+        {
+            m_outputDirectory = chooser.getResult();
+            m_directoryLabel.setEnabled(true);
+            m_directoryLabel.setText(m_outputDirectory.getFullPathName(), dontSendNotification);
+        }
+        else
+        {
+            m_sameDirectoryButton.setToggleState(true, dontSendNotification);
+            m_directoryLabel.setText(noneSelected, dontSendNotification);
+        }
+    };
 
     addSpacer();
 }
@@ -116,7 +156,11 @@ void MainComponent::convert(const StringArray &files) const
 
         AbletonDeviceGroupReader reader(file);
 
-        auto sitalaKit = file.getParentDirectory().getFullPathName() +
+        auto path = m_sameDirectoryButton.getToggleState() ?
+                    file.getParentDirectory().getFullPathName() :
+                    m_outputDirectory.getFullPathName();
+
+        auto sitalaKit = path +
                          File::getSeparatorChar() +
                          file.getFileNameWithoutExtension() +
                          ".sitala";
