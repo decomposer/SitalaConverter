b215fceef53f4fc50f722b4e38b51968f3ed7a6a
Scott Wheeler <scott@directededge.com>
2022-08-30 07:14:15 +0200

    Create simple component classes with SizeHints and start adding directory options

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -5,6 +5,18 @@
 
 class MainComponent : public LayoutManagedComponent, public FileDragAndDropTarget
 {
+    class Label : public juce::Label, public SizeHint
+    {
+    public:
+        Label(const String &text = String());
+    };
+
+    class ToggleButton : public juce::ToggleButton, public SizeHint
+    {
+    public:
+        ToggleButton(const String &text = String(), bool checked = false);
+    };
+
 public:
     MainComponent();
     ~MainComponent() override;
@@ -15,12 +27,18 @@ public:
     void fileDragEnter(const StringArray &files, int x, int y) override;
     void fileDragExit(const StringArray &files) override;
     void filesDropped(const StringArray &files, int x, int y) override;
+
 private:
     void setDragging(bool d);
     void convert(const StringArray &files) const;
 
     Label m_dropLabel;
     ToggleButton m_embedButton;
+
+    ToggleButton m_sameDirectoryButton;
+    ToggleButton m_specificDirectoryButton;
+
+    Label m_directoryLabel;
 
     bool m_dragging = false;
 
